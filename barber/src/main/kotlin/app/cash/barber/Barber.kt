537017ade93575150e23d62e7b754a2bc219e246
Andrew (Paradi) Alexander <adrw@squareup.com>
2019-07-31 14:21:43 -0400

    Documentation and API review (#20)

--- barber/src/main/kotlin/app/cash/barber/Barber.kt
+++ barber/src/main/kotlin/app/cash/barber/Barber.kt
@@ -5,9 +5,9 @@ import app.cash.barber.models.DocumentData
 import app.cash.barber.models.DocumentTemplate
 import app.cash.barber.models.Locale
 
-interface Barber<C : DocumentData, D : Document> {
-  /**
-   * @return a [Document] with the values of a [DocumentData] instance rendered in the [DocumentTemplate]
-   */
-  fun render(documentData: C, locale: Locale): D
+/**
+ * Uses a template to turn document datas into a rendered document.
+ */
+interface Barber<DD : DocumentData, D : Document> {
+  fun render(documentData: DD, locale: Locale): D
 }
--- barber/src/main/kotlin/app/cash/barber/BarberException.kt
+++ barber/src/main/kotlin/app/cash/barber/BarberException.kt
@@ -1,17 +1,28 @@
 package app.cash.barber
 
 /**
- * @return problems: a list of Strings describing the error
- * Can point to multiple problems
+ * An error, typically presented at application startup, that describes an inconsistency between the
+ * input document data types, the templates, and the target document types.
  *
- * Example
- * |output field 'subject' uses 'totally_invalid_field' but 'RecipientReceipt' has no such field
- * |  {sender} sent you {totally_invalid_field}
- * |  valid fields are: sender, amount, cancelUrl, deposit_expected_at
+ * For example this exception is raised if a document data lacks an input field that a template
+ * expects, or if a template lacks an output field that a document expects.
  *
- * TODO make exceptions typed so the above example could be parsed by a client and show rich feedback
- *  for DocumentTemplate writers
+ * Example:
+ *
+ * ```
+ * Problems
+ * 1) Attempted to install DocumentTemplate with a DocumentData not specified in the DocumentTemplate source.
+ * DocumentTemplate.source: class app.cash.barber.examples.RecipientReceipt
+ * DocumentData: class app.cash.barber.examples.SenderReceipt
+ *
+ * 2) Missing variable [sender] in DocumentData [class app.cash.barber.examples.SenderReceipt] for DocumentTemplate field [{{sender}} sent you {{amount}}. It will be available at {{ deposit_expected_at }}. Cancel here: {{ cancelUrl }}]
+ *
+ * 3) Unused DocumentData variable [recipient] in [class app.cash.barber.examples.SenderReceipt] with no usage in installed DocumentTemplate Locales:
+ * [Locale=en-US]
+ * ```
  */
+// TODO make exceptions typed so the above example could be parsed by a client and show rich
+//  feedback for DocumentTemplate writers
 class BarberException(
   val problems: List<String>
 ) : IllegalStateException() {
