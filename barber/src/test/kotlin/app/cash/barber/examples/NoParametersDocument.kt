c7eef598241c5d5c1a4f7dfbdc38192726e66335
Wesley Kim <wesley@squareup.com>
2019-09-11 16:44:06 -0400

    Add validations for Documents with no fields

---
+++ barber/src/test/kotlin/app/cash/barber/examples/NoParametersDocument.kt
@@ -0,0 +1,17 @@
+package app.cash.barber.examples
+
+import app.cash.barber.models.Document
+import app.cash.barber.models.DocumentData
+import app.cash.barber.models.DocumentTemplate
+import app.cash.barber.models.Locale
+
+class NoParametersDocument : Document
+
+class EmptyDocumentData : DocumentData
+
+val noParametersDocumentTemplate = DocumentTemplate(
+  fields = mapOf(),
+  source = EmptyDocumentData::class,
+  targets = setOf(NoParametersDocument::class),
+  locale = Locale.EN_US
+)
