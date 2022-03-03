9d1b5d791d453ba6bf3528dd4fab510e9b89690b
Andrew Alexander <adrw@squareup.com>
2022-03-03 14:26:44 -0500

    Don't throw exception on invalid URL, escape instead

--- barber/src/main/kotlin/app/cash/barber/BarberHttpUrlMustacheFactory.kt
+++ barber/src/main/kotlin/app/cash/barber/BarberHttpUrlMustacheFactory.kt
@@ -3,16 +3,27 @@ package app.cash.barber
 import app.cash.barber.models.BarberFieldEncoding.STRING_HTTPURL
 import com.github.mustachejava.DefaultMustacheFactory
 import com.github.mustachejava.MustacheException
+import com.github.mustachejava.util.HtmlEscaper
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import java.io.Writer
 
 /**
  * Mustache Factory that handles [STRING_HTTPURL] fields and doesn't apply the HTML escaping
- *  present in [DefaultMustacheFactory]
+ *  present in [DefaultMustacheFactory] if it's a valid OkHttp.HttpUrl
  */
 class BarberHttpUrlMustacheFactory : DefaultMustacheFactory() {
   override fun encode(value: String?, writer: Writer?) {
-    value?.let { writer?.write(it.toHttpUrl().toString()) }
-      ?: throw MustacheException("Null Writer. Failed to encode value: $value")
+    writer?.let { w ->
+      value?.let { v ->
+        try {
+          // Escape any non-URL valid characters if the value is a valid URL
+          w.write(v.toHttpUrl().toString())
+        } catch (e: IllegalArgumentException) {
+          // Else, assume that this is not a valid URL
+          // Escape the entire string and don't throw an exception
+          HtmlEscaper.escape(v, w)
+        }
+      } ?: throw MustacheException("Null Writer. Failed to encode value: $value")
+    }
   }
 }
